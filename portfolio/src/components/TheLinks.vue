7c0b7f6abf2400d1f53bb09388ef8f9b025cb9bd
Salledelavage <salledelavager@gmail.com>
2023-06-19 12:26:06 -0400

    save link section

--- portfolio/src/components/TheLinks.vue
+++ portfolio/src/components/TheLinks.vue
@@ -7,8 +7,8 @@ import LinksItem from './WelcomeItem.vue'
     <LinksItem>
 
         <template #icon>
-            <a href="https://discord.com/channels/769577020299935795/769577020920561667">
-              <img alt="Discord Logo" class="logo" src="@/assets/discord-logo.png" width="150" height="110" />
+            <a href="https://discord.com/channels/769577020299935795/769577020920561667" target="_blank" rel="noopener">
+              <img alt="Discord Logo" class="logo" src="@/assets/discord-logo.png" width="50" height="50" />
             </a>
         </template>
 
@@ -20,8 +20,8 @@ import LinksItem from './WelcomeItem.vue'
     <LinksItem>
 
         <template #icon>
-            <a href="https://open.spotify.com/">
-                <img alt="Spotify Logo" class="logo" src="@/assets/spotifylogo.png" width="160" height="100" />
+            <a href="https://open.spotify.com/" target="_blank" rel="noopener">
+                <img alt="Spotify Logo" class="logo" src="@/assets/spotifylogo.png" width="50" height="35" />
             </a>
         </template>
 
@@ -31,11 +31,11 @@ import LinksItem from './WelcomeItem.vue'
 
     <LinksItem>
 
-        <template #icon>
-            <a href="">
-                <img alt="Reddit Logo" class="logo" src="@/assets/reddit-logo.png" width="150" height="120" />
-            </a>
-        </template>
+            <template #icon>
+                <a href="https://www.reddit.com/" target="_blank" rel="noopener">
+                    <img alt="Reddit Logo" class="logo" src="@/assets/reddit-logo.png" width="50" height="50" />
+                </a>
+            </template>
 
         <template #heading>Reddit</template>
 
@@ -44,8 +44,8 @@ import LinksItem from './WelcomeItem.vue'
     <LinksItem>
 
         <template #icon>
-            <a href="">
-                <img alt="Reddit Logo" class="logo" src="@/assets/github-mark-white.png" width="100" height="100" />
+            <a href="https://github.com/grosheth" target="_blank" rel="noopener">
+                <img alt="Reddit Logo" class="logo" src="@/assets/github-mark-white.png" width="30" height="30" />
             </a>
         </template>
 
--- portfolio/src/components/TheWelcome.vue
+++ portfolio/src/components/TheWelcome.vue
@@ -3,8 +3,6 @@ import WelcomeItem from './WelcomeItem.vue'
 import DocumentationIcon from './icons/IconDocumentation.vue'
 import ToolingIcon from './icons/IconTooling.vue'
 import EcosystemIcon from './icons/IconEcosystem.vue'
-import CommunityIcon from './icons/IconCommunity.vue'
-import SupportIcon from './icons/IconSupport.vue'
 </script>
 
 <template>
@@ -44,31 +42,4 @@ import SupportIcon from './icons/IconSupport.vue'
 
   </WelcomeItem>
 
-  <!-- <WelcomeItem>
-    <template #icon>
-      <CommunityIcon />
-    </template>
-    <template #heading>Ecosystem</template>
-
-    Get official tools and libraries for your project:
-    <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">Pinia</a>,
-    <a href="https://router.vuejs.org/" target="_blank" rel="noopener">Vue Router</a>,
-    <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener">Vue Test Utils</a>, and
-    <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener">Vue Dev Tools</a>. If
-    you need more resources, we suggest paying
-    <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Awesome Vue</a>
-    a visit.
-  </WelcomeItem> -->
-
-
-  <!-- <WelcomeItem>
-    <template #icon>
-      <SupportIcon />
-    </template>
-    <template #heading>Support Vue</template>
-
-    As an independent project, Vue relies on community backing for its sustainability. You can help
-    us by
-    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
-  </WelcomeItem> -->
 </template>
