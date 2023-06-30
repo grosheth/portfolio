0543dfd6ad0111e7b98588cbef164a51fec34fbb
Salledelavage <salledelavager@gmail.com>
2023-06-29 20:10:00 -0400

    test

--- portfolio/src/components/ProjectsItem.vue
+++ portfolio/src/components/ProjectsItem.vue
@@ -1,16 +1,19 @@
 <template>
-    <div class="item">
-      <i>
-        <slot name="icon"></slot>
-      </i>
-      <div class="details">
-        <h3>
-          <slot name="heading"></slot>
-        </h3>
-        <p>
-          <slot name="paragraph"></slot>
-        </p>
-        <slot></slot>
-      </div>
-    </div>
-  </template>
+  <div>
+    <b-card
+      title="Card Title"
+      img-src="https://picsum.photos/600/300/?image=25"
+      img-alt="Image"
+      img-top
+      tag="article"
+      style="max-width: 20rem;"
+      class="mb-2"
+    >
+      <b-card-text>
+        Some quick example text to build on the card title and make up the bulk of the card's content.
+      </b-card-text>
+
+      <b-button href="#" variant="primary">Go somewhere</b-button>
+    </b-card>
+  </div>
+</template>
